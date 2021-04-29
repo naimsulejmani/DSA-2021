2a0ea7c8e1c163c8f5fe4df9d0ccfa15b0f9a71e
naimsulejmani <naim.sulejmani@gmail.com>
2021-04-29 20:33:29 +0200

    Node

---
+++ src/cacttus/education/dsa/Java_8_trees/BinarySearchTree.java
@@ -0,0 +1,28 @@
+package cacttus.education.dsa.Java_8_trees;
+
+import java.util.Comparator;
+
+//public class LinkedList<T> { private Node<T> head;}
+public class BinarySearchTree {
+    private Node root;
+    private int counter;
+
+    public BinarySearchTree() {
+        counter = 0;
+    }
+
+    public int size() {
+        return counter;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(int key) {
+
+    }
+
+
+
+}
